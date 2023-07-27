8d3c5a507dbdb93c758556bc11bd60dacdafb7ae
wdlea <83631101+wdlea@users.noreply.github.com>
2023-07-28 08:28:44 +1200

    Implemented a simple generic tuple

---
+++ Packets/tuple/tuple.cpp
@@ -0,0 +1,4 @@
+Tuple<type1, type2>::Tuple<type1, type2>(type1 a, type2 b){
+    this.a = a;
+    this.b = b;
+}
---
+++ Packets/tuple/tuple.h
@@ -0,0 +1,11 @@
+#ifndef TUPLE_H
+#define TUPLE_H
+
+struct Tuple<type1, type2>{
+    public:
+        type1 a;
+        type2 b;
+        Tuple<type1, type2>();
+}
+
+#endif
