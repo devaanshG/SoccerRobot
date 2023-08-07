e58ce2036c94991a512256d2131b12e6a5e7d4ca
devaanshG <devaanshnz@outlook.com>
2023-08-07 12:39:20 +1200

    Ball finding code
    Finds ball using approximated ball heading only, not using velocity yet.

--- test/ball_test/sketch_aug1a/sketch_aug1a.ino
+++ test/ball_test/sketch_aug1a/sketch_aug1a.ino
@@ -28,15 +28,16 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (map(analogRead(IR1), 1023, 300, 0, 1) == 0 && map(analogRead(IR2), 1023, 300, 0, 1) == 0 && map(analogRead(IR3), 1023, 300, 0, 1) == 0 && map(analogRead(IR4), 1023, 300, 0, 1) == 0) {
+  if (map(analogRead(IR1), 1023, 300, 0, 100) == 0 && map(analogRead(IR2), 1023, 300, 0, 100) == 0 && map(analogRead(IR3), 1023, 300, 0, 100) == 0 && map(analogRead(IR4), 1023, 300, 0, 100) == 0) {
     Serial.println("BALL NOT FOUND");
   } else {
-    int sensor1Reading = map(analogRead(IR1), 1023, 300, 0, 1);
-    int sensor2Reading = map(analogRead(IR2), 1023, 300, 0, 1);
-    int sensor3Reading = map(analogRead(IR3), 1023, 300, 0, 1);
-    int sensor4Reading = map(analogRead(IR4), 1023, 300, 0, 1);
+    int sensor1Reading = map(analogRead(IR1), 1023, 300, 0, 100);
+    int sensor2Reading = map(analogRead(IR2), 1023, 300, 0, 100);
+    int sensor3Reading = map(analogRead(IR3), 1023, 300, 0, 100);
+    int sensor4Reading = map(analogRead(IR4), 1023, 300, 0, 100);
 
-    ballDir = max(sensor1Reading, max(sensor2Reading, max(sensor3Reading, sensor4Reading))); // Find the sensor with the highest reading (I.e. the sensor that is most proximal to the ball)
+    ballDir = sensor1Reading * 90.0f / 100.0f + sensor2Reading * 30.0f / 100.0f + sensor3Reading * -30.0f / 100.0f + sensor4Reading * -90.0f / 100.0f; // Find the sensor with the highest reading (I.e. the sensor that is most proximal to the ball)
+    ballDir = map(ballDir, -30, 30, -90, 90);
 
     ballDelta = ballDir - prevDir; // Find
     ballVel = ballDelta - DeltaTime();
