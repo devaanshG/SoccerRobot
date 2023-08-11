013081faddf177769d0cbedd74a83221b13e95db
devaanshG <devaanshnz@outlook.com>
2023-08-11 23:29:12 +1200

    Added self-righting test. Updated magnetometer offsets

---
+++ test/SelfRightingTest/SelfRightingTest.ino
@@ -0,0 +1,49 @@
+#include "src/BallSensor/BallSensor.h"
+#include "src/ColourSensor/ColourSensor.h"
+#include "src/Magnetometer/magnetometer.h"
+#include "src/motor/Motors.h"
+
+const int GOAL_HEADING = 0;
+
+enum FieldColour{
+  OutWhite,
+  GoalBlack,
+  LightGreen,
+  DarkGreen,
+  MidfieldGreen
+};
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  ColourSensor::init();
+  Motors::init();
+  Magnetometer::Init();
+  while(!Serial) {
+    delay(10);
+  }
+  Serial.println("<------ SELF RIGHTING TEST ------>");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int COL = ColourSensor::get_current_colour_ID();
+  int theta = (int)Magnetometer::GetHeading();
+
+  Serial.print("COLOUR: ");
+  Serial.print(COL);
+  Serial.print(" HEADING: ");
+  Serial.println(theta);
+  while(COL != (int)FieldColour::GoalBlack) {
+    int theta = (int)Magnetometer::GetHeading();
+    Motors::MoveMotors(-(90+theta), 255, 0);
+  }
+
+  while (theta != GOAL_HEADING) {
+      if (theta < GOAL_HEADING) {
+        Motors::MoveMotors(0, 0, 255);
+      } else {
+        Motors::MoveMotors(0, 0, -255);
+      }
+    }
+}
---
+++ test/SelfRightingTest/src/ColourSensor/ColourSensor.cpp
@@ -0,0 +1,56 @@
+#include "Arduino.h"
+#include "ColourSensor.h"
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_TCS34725.h"
+
+uint16_t ColourSensor::r = 0;
+uint16_t ColourSensor::g = 0;
+uint16_t ColourSensor::b = 0;
+uint16_t ColourSensor::c = 0;
+Colour ColourSensor::col = Colour();
+
+Colour ColourSensor::colours[5] = {
+    *new Colour(5029, 7977, 9186),//outside
+    *new Colour(457, 590, 697),//goal black
+    *new Colour(2229, 3391, 2339),//green 1
+    *new Colour(1744, 3350, 2660),//green 2
+    *new Colour(546, 919, 1027)//midfield
+};
+
+Adafruit_TCS34725 ColourSensor::sensor = *new Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
+
+void ColourSensor::init(){
+
+    
+    sensor = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
+
+    if(!sensor.begin(41, &Wire)){
+        Serial.println("tcs not found");
+    }
+
+    
+}
+void ColourSensor::update_colours(){
+    ColourSensor::sensor.getRawData(&r, &g, &b, &c);
+}
+
+int ColourSensor::get_current_colour_ID(){
+    update_colours();
+    col = Colour(ColourSensor::r, ColourSensor::g, ColourSensor::b);//get RGB
+    Serial.println(col.ToString());
+
+    long int minDist = 0xFFFFFFFF;
+    int closestCol = -1;
+    for(int i = 0; i < 5; i++){
+        int dist = col.DistanceFrom(colours[i]);
+        if(dist < minDist){
+            closestCol = i;
+            minDist = dist;
+        }
+    }
+
+    const int ColourMaximumDistance = 0xFFFFFFF;//big number, this is just here in case we need to do something to do with filtering out other colours later
+    return (minDist < ColourMaximumDistance ? closestCol : -1);
+}
